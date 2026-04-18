61c1c8d7f60aad2296657bee62d63bc0ccb148e3
Victor-Suander <victors.empresa@gmail.com>
2026-04-17 21:53:09 -0300

    chore(init): spring boot project with h2 and initial endpoint

---
+++ src/main/java/com/victor/hireFlow/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.victor.hireFlow.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "HireFlow backend is running!";
+    }
+}
---
+++ src/test/java/com/victor/hireFlow/HireFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.hireFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
